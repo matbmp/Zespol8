8973dcc384dde11f7eb5be3dc3eb7241c87529ac
matbmp <mbiernacki531@gmail.com>
2023-05-30 00:57:14 +0200

    save changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
-<script>
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+<script lang="ts">
+	import '../theme.css';
+	//import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 
 	// This contains the bulk of Skeletons required styles:
 	// NOTE: this will be renamed skeleton.css in the v2.x release.
@@ -9,8 +10,58 @@
 	import '../app.postcss';
 
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import { AppBar, AppShell, storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
+	const buttons = [
+		{
+			text: 'Zespół',
+			href: '#team'
+		},
+		{
+			text: 'Harmonogram',
+			href: '#harmonogram'
+		},
+		{
+			text: 'Przebieg',
+			href: '#progress'
+		},
+		{
+			text: 'Raporty',
+			href: '#raports'
+		},
+		{
+			text: 'Dokumentacja',
+			href: '#docs'
+		}
+	];
 </script>
 
-<slot />
+<AppShell slotHeader="shadow-lg sticky top-0 z-10">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<img src="/umk.svg" alt="" width="40" height="40" />
+			</svelte:fragment>
+			Programowanie zespołowe - zespół 8
+			<svelte:fragment slot="trail">
+				<div class="hidden lg:block">
+					{#each buttons as button}
+						<a
+							href={button.href}
+							class="btn mx-2 variant-filled-primary"
+							on:click={(e) =>
+								document
+									.getElementById(new URL(e.currentTarget.href).hash.replace('#', ''))
+									?.scrollIntoView({behavior: 'smooth'}, )}
+						>
+							{button.text}
+						</a>
+					{/each}
+				</div>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<slot />
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,117 @@
 <script lang="ts">
-	import Icon from 'virtual:icons/mdi/rocket-launch-outline';
-	import type { PopupSettings } from '@skeletonlabs/skeleton';
-	import { popup } from '@skeletonlabs/skeleton';
-
-	const popupClick: PopupSettings = {
-		event: 'click',
-		target: 'popupClick',
-		placement: 'top'
-	};
+	import { teammates, harmonogram, progress, raports } from '../assets/data';
+	import { Step, Stepper } from '@skeletonlabs/skeleton';
+	import Account from 'virtual:icons/mdi/account-outline';
+	import Pdf from 'virtual:icons/mdi/file-pdf-box';
 </script>
 
-<div class="p-4 text-lg">
-	<h1 class="text-2xl">Welcome to SvelteKit</h1>
-	This template includes:
-	<ul>
-		<li class="my-2">
-			<button class="rounded-xl bg-blue-400 text-white p-2 px-4">Tailwind</button>
-		</li>
-		<li><button class="btn variant-filled" use:popup={popupClick}>SkeletonUI with Floating UI</button></li>
-		<li>unplugin-icons with Material Design Icon pack <Icon class="inline-block w-10 h-10" /></li>
-	</ul>
+<div id='team' class="contain variant-filled-secondary">
+	<div class="w-full lg:w-2/3 mx-auto">
+		<div class="head">Nasz zespół</div>
+
+		<div
+			class="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-12 md:gap-6 gap-3"
+		>
+			{#each teammates as teammate}
+				<div class="flex flex-col card items-center p-4 variant-outline-surface shadow-xl">
+					<Account class="text-9xl text-primary" />
+					<div class="text-xl font-bold pt-4">{teammate.name}</div>
+					<div class="text-lg">{teammate.role}</div>
+				</div>
+			{/each}
+		</div>
+	</div>
 </div>
 
-<div class="card p-4 variant-filled-primary" data-popup="popupClick">
-	<p>Click Content</p>
-	<div class="arrow variant-filled-primary" />
+<div id='harmonogram' class="contain">
+	<div class="w-full lg:w-2/3 mx-auto">
+		<div class="head">Harmonogram</div>
+
+		<div class="h-[30rem] md:h-[25rem] lg:h-[20rem] flex flex-col">
+			<div class="my-auto">
+				<Stepper
+					stepTerm=""
+					buttonBackLabel="←"
+					buttonBack="variant-filled shadow text-3xl"
+					buttonNextLabel="→"
+					buttonNext="variant-filled shadow text-3xl"
+					buttonCompleteLabel="Koniec"
+				>
+					{#each harmonogram as step}
+						<Step
+							regionHeader="h-60 md:h-40 lg:h-32 text-lg lg:text-xl xl:text-2xl flex flex-col"
+							regionContent="text-lg lg:text-2xl badge variant-ringed-surface shadow-lg py-2 px-4"
+						>
+							<svelte:fragment slot="header"
+								><div class="my-auto">{step.title}</div></svelte:fragment
+							>
+							{step.date}
+						</Step>
+					{/each}
+				</Stepper>
+			</div>
+		</div>
+	</div>
 </div>
+
+<div id='progress' class="contain">
+	<div class="w-full lg:w-2/3 mx-auto">
+		<div class="head">Przebieg prac</div>
+
+		<div class="border-l-4 pl-4 border-primary-500 relative">
+			{#each progress as step}
+				<div class="py-2">
+					<div class={`rounded-full h-5 w-5 -ms-7 absolute variant-filled-primary`} />
+					<div>
+						{#if step.date !== undefined}
+							{step.date}
+						{/if}
+					</div>
+					{#if step.title !== undefined}
+						<div class="font-bold pb-4">{step.title}</div>
+					{/if}
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<div id='raports' class="contain variant-filled-secondary">
+	<div class="w-full lg:w-2/3 mx-auto">
+		<div class="head">Raporty semestralne</div>
+
+		<div
+			class="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-12 md:gap-6 gap-3"
+		>
+			{#each raports as raport}
+				<a
+					href={raport.download}
+					download={raport.download}
+					class="flex flex-col card items-center p-4 variant-outline-primary shadow-lg"
+				>
+					<Pdf class="text-9xl text-surface" />
+					{raport.name}
+				</a>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<div id='docs' class="contain">
+	<div class="w-full lg:w-2/3 mx-auto">
+		<div class="head">Dokumentacja</div>
+	</div>
+</div>
+
+<style lang='postcss'>
+	.contain{
+		@apply shadow-lg py-6 lg:py-16 px-2
+	}
+	.head{
+		@apply flex justify-center text-4xl lg:text-6xl pb-12
+	}
+
+	.contain2{
+
+	}
+</style>
